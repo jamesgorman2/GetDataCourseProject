aa32b269212245d6ce906d20c022675f0639b008
James Gorman <jamesgorman2@gmail.com>
2014-11-22 11:34:53 +1100

    Comments and README

---
+++ README.md
@@ -0,0 +1,32 @@
+# Getting and Cleaning Data Course Project
+
+This is the code submition for the [Getting and Cleaning Data Course Project](https://class.coursera.org/getdata-009/human_grading/view/courses/972587/assessments/3).
+It takes data from [Human Activity Recognition Using Smartphones Data Set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+(HAR) and calculates the mean of a subset of measurements.
+
+## Files
+
+* [`README.md`](https://github.com/jamesgorman2/GetDataCourseProject/blob/master/README.md) - this file
+* [`run_analysis.R`](https://github.com/jamesgorman2/GetDataCourseProject/blob/master/run_analysis.R) - an 
+  [R](http://www.r-project.org/) script that downloads the HAR data and performs the calculations, described below.
+* [`CodeBook.md`](https://github.com/jamesgorman2/GetDataCourseProject/blob/master/CodeBook.md) - describes 
+  the transformations applied to the original HAR data and the output of `run_analysis.R`. 
+* [`uci_har_means_long.txt`](https://github.com/jamesgorman2/GetDataCourseProject/blob/master/uci_har_means_long.txt) - 
+  the output of `run_analysis.R` as described in `CodeBook.md`.
+
+## Reproducing the output
+
+Running the project requires [R](http://www.r-project.org/). 
+
+To reproduce the data run:
+
+```
+$ r
+> source('run_analysis.R')
+```
+
+This will download the original data, perform the calculations and output the data to `uci_har_means_long.txt`.
+The data is only downloaded if it is not present. `dyplyr` will produce masking warnings that can be
+ignored. All operations happen in the current working directory.
+
+The code is currently unoptimised for readability.
--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,42 @@
-library(dplyr)
-library(plyr)
+# This code will download UCI HAR data, normalise it, extract and calculate
+# the desired means, then export the resulting data.
+#
+# The output data format is 'tall' - it is in the format provided by `melt`,
+# where the interesting variables are output in the column 'variable', with 
+# the mean values in 'mean'.
+#
+# This has not been optimised - all data is loaded from the original data, 
+# then it is filtered and the new data calculated. This is to improve readability
+# at the cost of performance.
+#
+# When run, `dplyr` will produce masking warnings that can be ignored.
+#
+# It is broken into three sections: loading, calculating means and running.
+
+library(plyr); library(dplyr) # this ordering is required to prevent incorrect masking
+library(reshape2)
 library(stringr)
-  
+
+
+###############################################################################
+# Constants and psuedo-constant functions
+###############################################################################
+
+data.url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zip.file <- "UCI_HAR.zip"
 data.path <- "UCI HAR Dataset"
-activity.name.file <- paste(data.path, "activity_labels.txt", sep="/")
-measurement.name.file <- paste(data.path, "features.txt", sep="/")
+activity.name.file <- paste0(data.path, "/activity_labels.txt")
+measurement.name.file <- paste0(data.path, "/features.txt")
 
 DataSetPath <- function(data.set) {
   paste(data.path, data.set, sep="/")
 }
 
-activityFile <- function(data.set) {
+ActivityFile <- function(data.set) {
   paste0(DataSetPath(data.set), "/y_", data.set, ".txt")
 }
 
-subjectFile <- function(data.set) {
+SubjectFile <- function(data.set) {
   paste0(DataSetPath(data.set), "/subject_", data.set, ".txt")
 }
 
@@ -23,10 +44,14 @@ MeasurementsFile <- function(data.set) {
   paste0(DataSetPath(data.set), "/X_", data.set, ".txt")
 }
 
+
+###############################################################################
+# Functions to download, read and normalise the original data.
+###############################################################################
+
 DownloadData <- function(skip.if.exists) {
-  if (!skip.if.exists && !file.exists(zip.file)) {
-    download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-                  zip.file, mode="wb")
+  if (!skip.if.exists || !file.exists(zip.file)) {
+    download.file(data.url, zip.file, mode="wb")
   }
 }
 
@@ -40,6 +65,13 @@ LoadRawNames <- function() {
 }
 
 ReLabelNames <- function(names) {
+  # Column names are split into measurement (eg BodyAcc), dimention (eg X) and
+  # calculation (eg mean), extra characters removed, reordered and rendered in
+  # camelCase. Further normalisation is done using `make.names` - this forces 
+  # uniqueness and removes illegal characters from names not treated in the first
+  # round. We are not yet interested in the names/measuments that are modified
+  # so they are left as is.
+  
   Normalise <- function(x) {
     elements <- str_split(x, "-")[[1]]
     measurement <- paste0(tolower(substring(elements[1], 2, 2)), substring(elements[1], 3))
@@ -60,6 +92,8 @@ ReLabelNames <- function(names) {
 }
 
 ReLabelActivities <- function(activities) {
+  # Activity names are split to include white space and are put in sentence case
+  
   Normalise <- function(x) {
     paste0(substring(x, 1, 1),  gsub("_+", " ", tolower(substring(x, 2))))
   }
@@ -71,34 +105,46 @@ LoadRawActivtyNames <- function() {
   read.table(activity.name.file, col.names=c("id", "activity"))
 }
 
-LoadActivities <- function(data.set) {
+LoadActivitiesAsLabels <- function(data.set) {
   activity.names <- ReLabelActivities(LoadRawActivtyNames())
-  raw.activities <- read.table(activityFile(data.set), col.names="id")
+  raw.activities <- read.table(ActivityFile(data.set), col.names="id")
   activities <- join(raw.activities, activity.names, by="id")
   activities[, "activity", drop=FALSE]
 }
 
-LoadMeasurements <- function(data.set) {
+LoadMeasurementsWithColumnNames <- function(data.set) {
   names <- ReLabelNames(LoadRawNames())
-  read.table(MeasurementsFile(data.set), col.names=names)
+  classes <- sapply(read.table(MeasurementsFile(data.set), nrows=5), class)
+  read.table(MeasurementsFile(data.set), col.names=names, colClasses = classes)
 }
 
 LoadSubjects <- function(data.set) {
-  subjects <- read.table(subjectFile(data.set), col.names=c("subject"))
+  subjects <- read.table(SubjectFile(data.set), col.names=c("subject"))
 }
 
 LoadDataSet <- function(data.set) {
-  measurements <- LoadMeasurements(data.set)
-  activities <- LoadActivities(data.set)
+  # Loads an individual named data set (eg test or train).
+  
+  measurements <- LoadMeasurementsWithColumnNames(data.set)
+  activities <- LoadActivitiesAsLabels(data.set)
   subjects <- LoadSubjects(data.set)
   cbind(measurements, activities, subjects)
 }
   
 LoadData <- function() {
+  # Load the test and training data from UCI HAR. Only download the data if it
+  # has not been downloaded. The current working directory is used to store 
+  # the raw files.
+
   DownloadData(TRUE)
   UnzipData()
   rbind(LoadDataSet("test"), LoadDataSet("train"))
 }
+
+
+###############################################################################
+# Functions to extract and transform the subset of data we are interested in. 
+###############################################################################
 
 IsMeanOrStdColumn <- function(name) {
   grepl("(Mean|Std)$", name) 
@@ -109,14 +155,29 @@ KeyNames <- function(all.names) {
 }
 
 KeyMeasurements <- function(all.measurements) {
+  # Return a table with the subset of measurements/columns in which we are interested,
+  # as defined by `KeyNames`. The two identifying columns, 'activity' and 'subject' are
+  # included in this.
+  
   all.measurements[, KeyNames(names(all.measurements))]
 }
 
 FindMeans <- function(measurements) {
+  # Find the mean of each measurement, grouped by 'activity' and 'subject'. This
+  # returnes a melted table, where the measurements are per-row, not per-column.
+  
   melted <- melt(measurements, id=c("activity", "subject"))
   grouped <- group_by(melted, activity, subject, variable)
   summarise(grouped, mean=mean(value))
 }
 
+
+###############################################################################
+# Run the analysis
+#
+# This is a simple composition of the entry point functions provided above,
+# with the resulting table output as-is.
+###############################################################################
+
 means <- FindMeans(KeyMeasurements(LoadData()))
-write.table(means, "uci_har_means_long.txt",  row.name=FALSE)
+write.table(means, "uci_har_means_long.txt", row.name=FALSE)
